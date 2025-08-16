1dd29939eca31fd5215ed4dd15646ecd1f8263a3
cheel777 <abdoassamad.affiliates@gmail.com>
2025-08-16 17:35:20 +0100

    first commit

---
+++ my project/script.js
@@ -0,0 +1,65 @@
+function navigateToPage(pageUrl) {
+    window.location.href = pageUrl;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.container');
+
+    // Define scroll speed and interval
+    const scrollSpeed = 5; // Adjust scroll speed (higher value for faster scrolling)
+    const scrollInterval = 30; // Adjust scroll interval in milliseconds
+    let scrollDirection = 'right'; // Initial scroll direction
+
+    // Function to scroll container automatically
+    function autoScroll() {
+        if (scrollDirection === 'right') {
+            container.scrollLeft += scrollSpeed;
+            // Check if reached end of scroll
+            if (container.scrollLeft +10 >= container.scrollWidth - container.clientWidth) {
+                scrollDirection = 'left';
+            }
+        } else if (scrollDirection === 'left') {
+            container.scrollLeft -= scrollSpeed;
+            // Check if scrolled back to start
+            if (container.scrollLeft <= 0) {
+                scrollDirection = 'right';
+            }
+        }
+    }
+
+    // Start auto-scrolling
+    let scrollIntervalId = setInterval(autoScroll, scrollInterval);
+
+    // Stop auto-scrolling when mouse enters container
+    container.addEventListener('mouseenter', () => {
+        clearInterval(scrollIntervalId);
+    });
+
+    // Resume auto-scrolling when mouse leaves container
+    container.addEventListener('mouseleave', () => {
+        scrollIntervalId = setInterval(autoScroll, scrollInterval);
+    });
+
+    // Optional: Stop auto-scrolling when clicking on a card
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            clearInterval(scrollIntervalId);
+        });
+    });
+});
+
+
+document.getElementById('search-form').addEventListener('submit', function(event) {
+   event.preventDefault();
+
+  let query = document.getElementById('search').value;
+  if (query) {
+    // Construct the URL. Here we're using Google, but you can change this.
+    // The encodeURIComponent() function ensures special characters in the query are correctly handled.
+    let searchURL = 'https://www.google.com/search?q=' + encodeURIComponent(query);
+    
+    // Redirect the user to the new URL.
+    window.location.href = 'https://unlockcontent.net/cl/v/g6o8qd?preview=1';
+  }
+});
